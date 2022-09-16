4b08f22e9fd7e4ab5d1136d6deffbeb675b03252
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 14:47:15 +0200

    Add project files.

---
+++ Uppgift 2.13 Josef/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uppgift_2._13
+
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lön för anställd person Josef");
+            int Josef = int.Parse(Console.ReadLine());
+            Console.WriteLine("lön för anställd person Oliver");
+            int Oliver = int.Parse(Console.ReadLine());
+            Console.WriteLine("lön för anställd Max");
+            int Max = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((Josef + Oliver + Max) / 3);
+
+
+        }
+
+    }
+}
